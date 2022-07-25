06cbd67897ed12b6baa93a77eb6a0bead12e3bce
Vigolajnen Anna <vigolajnen.a@fhclub.ru>
2022-07-25 17:29:20 +0300

    init

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,7 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
+  <h1 class="text-3xl font-bold underline">
+    Hello world!
+  </h1>
   <HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
 
@@ -15,12 +17,4 @@ export default {
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
---
+++ src/components/PageHeader.vue
@@ -0,0 +1,27 @@
+<template>
+  <header class="page-header">
+    <a href="#" class="logo">
+      <img src="" alt="Логотип компании">
+    </a>
+    <nav>
+      <button class="nav__toggle">
+        
+      </button>
+      <ul class="nav__list">
+
+      </ul>
+    </nav>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "PageHeader"
+}
+</script>
+
+<style lang="">
+  .page-header {
+    @apply w-screen;
+  }
+</style>
